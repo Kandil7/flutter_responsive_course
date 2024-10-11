751ac88f7c2e1da1a5a482c3c8fa7416ed54a691
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2024-10-11 22:44:06 +0300

    Refactor screen_utils.dart

---
+++ lib/media_query_test/responsive_service/screen_utils.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/widgets.dart';
+
+class ScreenUtils {
+  static double _screenWidth = 360;
+  static double _screenHeight = 640;
+  static double _screenDensity = 1.0;
+  static TextScaler _textScaleFactor = TextScaler.noScaling;
+
+  static double get screenWidth => _screenWidth;
+  static double get screenHeight => _screenHeight;
+  static double get screenDensity => _screenDensity;
+  static TextScaler get textScaleFactor => _textScaleFactor;
+
+  static void init(context) {
+    _screenWidth = MediaQuery.of(context).size.width;
+    _screenHeight = MediaQuery.of(context).size.height;
+    _screenDensity = MediaQuery.of(context).devicePixelRatio;
+    _textScaleFactor = MediaQuery.of(context).textScaler;
+  }
+
+  static double getScaleWidth(double width) {
+    return width * _screenWidth / screenWidth;
+  }
+
+  static double getScaleHeight(double height) {
+    return height * _screenHeight / screenHeight;
+  }
+
+  static double getScaleText(double fontSize) {
+    return textScaleFactor.scale(fontSize);
+  }
+
+  static double getScaleSp(double fontSize) {
+    return getScaleText(fontSize);
+  }
+}
